6982e14c5166d858aad181b0cf089c34511bd2fe
amirmohammad <ahmadi.amirmohammad84@gmail.com>
2025-06-08 13:08:34 +0330

    fix-6+: edit user dashboard

--- app/user/orders/page.tsx
+++ app/user/orders/page.tsx
@@ -48,13 +48,13 @@ const Orders = ({ params }: any) => {
           <div className="sm:flex-auto">
             <h1 className="text-xl font-semibold text-gray-900">سبد محصولات</h1>
           </div>
-          <div className="mt-4 sm:mt-0 sm:mr-16 sm:flex-none">
+          {/* <div className="mt-4 sm:mt-0 sm:mr-16 sm:flex-none">
             <Link href="/products">
               <div className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">
                 اضافه کردن محصول
               </div>
             </Link>
-          </div>
+          </div> */}
         </div>
 
         <div className="mt-8 flex flex-col">
